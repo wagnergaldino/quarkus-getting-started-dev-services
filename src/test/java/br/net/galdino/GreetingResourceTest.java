5982f9f2d2bdc8ec73f22d0d9c1d0d15453b8ff4
Wagner Galdino <wagalwagal@gmail.com>
2024-07-24 16:19:55 -0300

    Exercício do website do Quarkus

---
+++ src/test/java/br/net/galdino/GreetingResourceTest.java
@@ -0,0 +1,20 @@
+package br.net.galdino;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class GreetingResourceTest {
+    @Test
+    void testHelloEndpoint() {
+        given()
+          .when().get("/hello/wagal")
+          .then()
+             .statusCode(200)
+             .body(is("Hello wagal from Quarkus REST"));
+    }
+
+}
